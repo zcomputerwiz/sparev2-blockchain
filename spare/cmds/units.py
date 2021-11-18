bfb8791718418f14d19a62984b1704badaa2c137
zcomputerwiz <zcomputerwiz@gmail.com>
2021-11-18 15:32:11 -0600

    replace xch with spare

--- spare/cmds/units.py
+++ spare/cmds/units.py
@@ -3,7 +3,7 @@ from typing import Dict
 # The rest of the codebase uses gravitons everywhere.
 # Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    "spare": 10 ** 12,  # 1 spare (XCH) is 1,000,000,000,000 graviton (1 trillion)
+    "spare": 10 ** 12,  # 1 spare (SPARE) is 1,000,000,000,000 graviton (1 trillion)
     "graviton": 1,
     "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin gravitons
 }
--- spare/wallet/puzzles/prefarm/make_prefarm_ph.py
+++ spare/wallet/puzzles/prefarm/make_prefarm_ph.py
@@ -8,8 +8,8 @@ from spare.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
 from spare.util.condition_tools import parse_sexp_to_conditions
 from spare.util.ints import uint32
 
-address1 = "txch15gx26ndmacfaqlq8m0yajeggzceu7cvmaz4df0hahkukes695rss6lej7h"  # Gene wallet (m/12381/8444/2/42):
-address2 = "txch1c2cguswhvmdyz9hr3q6hak2h6p9dw4rz82g4707k2xy2sarv705qcce4pn"  # Mariano address (m/12381/8444/2/0)
+address1 = "tspare15gx26ndmacfaqlq8m0yajeggzceu7cvmaz4df0hahkukes695rss6lej7h"  # Gene wallet (m/12381/8444/2/42):
+address2 = "tspare1c2cguswhvmdyz9hr3q6hak2h6p9dw4rz82g4707k2xy2sarv705qcce4pn"  # Mariano address (m/12381/8444/2/0)
 
 ph1 = decode_puzzle_hash(address1)
 ph2 = decode_puzzle_hash(address2)
@@ -30,7 +30,7 @@ def make_puzzle(amount: int) -> int:
     puzzle_hash = puzzle_prog.get_tree_hash()
 
     solution = "()"
-    prefix = "xch"
+    prefix = "spare"
     print("PH", puzzle_hash)
     print(f"Address: {encode_puzzle_hash(puzzle_hash, prefix)}")
 
