bbe20605ce7410c88704c8eb4282398831abde9f
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2021-11-28 19:17:39 -0600

    correct default ports

--- spare/wallet/derive_keys.py
+++ spare/wallet/derive_keys.py
@@ -7,7 +7,7 @@ from spare.util.ints import uint32
 # EIP 2334 bls key derivation
 # https://eips.ethereum.org/EIPS/eip-2334
 # 12381 = bls spec number
-# 8444 = Spare blockchain number and port number
+# 9444 = Spare blockchain number and port number
 # 0, 1, 2, 3, 4, 5, 6 farmer, pool, wallet, local, backup key, singleton, pooling authentication key numbers
 
 
